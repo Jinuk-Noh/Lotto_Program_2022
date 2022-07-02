33efee98f42fed9000a6f083ad1f1afeb2c7e4cc
Jinuk-Noh <beehonestfly@gmail.com>
2022-07-02 21:11:02 +0900

    * Lotto 조회 프로그램  - 화면 생성

---
+++ Lotto_Program_2022/Lotto_Program_2022/Model/LotNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto_Program_2022.Model
+{
+    public class LotNumber: Notifier
+    {
+        private int _Number;
+        public int Number 
+        {   get { return _Number; }
+            set {_Number = value; OnPropertyChanged("Number"); } 
+        }
+
+        private NumberStatus _RightStatus;
+        public NumberStatus RightStatus
+        {
+            get { return _RightStatus; }
+            set { _RightStatus = value; OnPropertyChanged("RightStatus"); }
+        }
+
+        public LotNumber(int lotNumber)
+        {
+            this.Number = lotNumber;
+            this.RightStatus = NumberStatus.Nothing;
+        }
+    }
+    
+    public enum NumberStatus
+    {
+        Nothing = 0,
+        Right = 1,
+        Fail = 2
+    }
+
+    public class LotNumbers
+    {
+        ObservableCollection<LotNumber> numbers = new ObservableCollection<LotNumber>();
+        public ObservableCollection<LotNumber> GetAllNumbers()
+        {
+            for (int i = 1; i < 46; i++)
+            {
+                numbers.Add(new LotNumber(i));
+            }
+
+            return numbers;
+        }
+    }
+
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ Lotto_Program_2022/Lotto_Program_2022/ViewModel/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lotto_Program_2022.Model;
+
+namespace Lotto_Program_2022.ViewModel
+{
+    public class ViewModel :Notifier
+    {
+        ObservableCollection<LotNumber> numbers = new ObservableCollection<LotNumber>();
+        public ObservableCollection<LotNumber> GetAllNumbers()
+        {
+            for (int i = 1; i < 46; i++)
+            {
+                numbers.Add(new LotNumber(i));
+            }
+
+            return numbers;
+        }
+    }
+
+}
